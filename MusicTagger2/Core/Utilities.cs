2a866c584f674adf280f3a943eeede97ac9022cf
Jan Toms - Amaroth <jantoms@seznam.cz>
2019-03-03 17:32:17 +0100

    Another cleaning of unused stuff.

--- MusicTagger2/Core/Utilities.cs
+++ MusicTagger2/Core/Utilities.cs
@@ -1,42 +1,18 @@
 ﻿using System.Text;
-using System.Xml;
 
 namespace MusicTagger2.Core
 {
     class Utilities
     {
-        /// <summary>
-        /// Adds a new element as child under provided node in give xml.
-        /// </summary>
-        /// <param name="xml"></param>
-        /// <param name="parent"></param>
-        /// <param name="name"></param>
-        /// <param name="innerText"></param>
-        /// <param name="comment"></param>
-        public static void XmlAddElement(XmlDocument xml, XmlNode parent, string name, string innerText, string comment)
-        {
-            XmlNode newNode = xml.CreateElement(name);
-            if (comment != "" && comment != null)
-            {
-                XmlComment newComment = xml.CreateComment(name + ": " + comment);
-                parent.AppendChild(newComment);
-            }
-            newNode.InnerText = innerText;
-            parent.AppendChild(newNode);
-        }
-
-        public static bool IsFileSupported(string path)
-        {
-            return path.ToLower().EndsWith(".mp3");
-        }
+        public static bool IsFileSupported(string filePath) => filePath.ToLower().EndsWith(".mp3");
 
         public static string GetTimeString(int timeInSeconds)
         {
-            int hours = timeInSeconds / 3600;
-            int minutes = (timeInSeconds % 3600) / 60;
-            int seconds = timeInSeconds % 60;
+            var hours = timeInSeconds / 3600;
+            var minutes = (timeInSeconds % 3600) / 60;
+            var seconds = timeInSeconds % 60;
 
-            StringBuilder result = new StringBuilder();
+            var result = new StringBuilder();
             result.Append(string.Format("{0}:", hours));
 
             if (minutes < 10)
