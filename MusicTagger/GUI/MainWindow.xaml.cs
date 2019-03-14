4de9005c29145fa12ee6951a98b109bddfe51a96
Jan Toms - Amaroth <jantoms@seznam.cz>
2019-03-14 15:14:06 +0100

    Fix column name

--- MusicTagger/GUI/MainWindow.xaml.cs
+++ MusicTagger/GUI/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace MusicTagger.GUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string CurrentVersionSignature = "Music Tagger 2.8.5";
+        private string CurrentVersionSignature = "Music Tagger 2.8.6";
         private string CurrentProjectFilePath = "";
 
         private Core.Core core = Core.Core.Instance;
