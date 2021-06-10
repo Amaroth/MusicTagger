3880823027a6f1c8cbb9f9043420df41bad5cb7f
Jan Toms - Amaroth <jantoms@seznam.cz>
2021-06-10 21:51:44 +0200

    Update packages.

--- MusicTagger/GUI/MainWindow.xaml.cs
+++ MusicTagger/GUI/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace MusicTagger.GUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string CurrentVersionSignature = "Music Tagger 2.9.0";
+        private string CurrentVersionSignature = "Music Tagger 2.9.1";
         private string CurrentProjectFilePath = "";
 
         private Core.Core core = Core.Core.Instance;
